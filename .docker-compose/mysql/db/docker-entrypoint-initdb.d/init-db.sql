3a5bd19d539e29950c929b5cce001fa800c9c777
Arvin He <arvin.he@softtek.com>
2020-07-10 09:58:48 +0800

    reformat sql doc

--- .docker-compose/mysql/db/docker-entrypoint-initdb.d/init-db.sql
+++ .docker-compose/mysql/db/docker-entrypoint-initdb.d/init-db.sql
@@ -5,22 +5,22 @@ SET NAMES UTF8;
 
 -- 初始化用户表
 CREATE TABLE IF NOT EXISTS `t_user` (
-  `id` bigint(20) NOT NULL AUTO_INCREMENT,
-  `username` varchar(32) COLLATE utf8_unicode_ci DEFAULT NULL,
-  `email` varchar(128) COLLATE utf8_unicode_ci DEFAULT NULL,
-  `nickname` varchar(16) COLLATE utf8_unicode_ci DEFAULT NULL,
-  `avatar` text COLLATE utf8_unicode_ci,
-  `password` varchar(512) COLLATE utf8_unicode_ci DEFAULT NULL,
-  `status` int(11) NOT NULL,
-  `roles` text COLLATE utf8_unicode_ci,
-  `type` int(11) NOT NULL,
-  `description` text COLLATE utf8_unicode_ci,
-  `create_time` bigint(20) DEFAULT NULL,
-  `update_time` bigint(20) DEFAULT NULL,
-  PRIMARY KEY (`id`),
-  UNIQUE KEY `username` (`username`),
-  UNIQUE KEY `email` (`email`),
-  KEY `idx_status` (`status`)
+    `id` bigint(20) NOT NULL AUTO_INCREMENT,
+    `username` varchar(32) COLLATE utf8_unicode_ci DEFAULT NULL,
+    `email` varchar(128) COLLATE utf8_unicode_ci DEFAULT NULL,
+    `nickname` varchar(16) COLLATE utf8_unicode_ci DEFAULT NULL,
+    `avatar` text COLLATE utf8_unicode_ci,
+    `password` varchar(512) COLLATE utf8_unicode_ci DEFAULT NULL,
+    `status` int(11) NOT NULL,
+    `roles` text COLLATE utf8_unicode_ci,
+    `type` int(11) NOT NULL,
+    `description` text COLLATE utf8_unicode_ci,
+    `create_time` bigint(20) DEFAULT NULL,
+    `update_time` bigint(20) DEFAULT NULL,
+    PRIMARY KEY (`id`),
+    UNIQUE KEY `username` (`username`),
+    UNIQUE KEY `email` (`email`),
+    KEY `idx_status` (`status`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
 -- 初始化用户数据（用户名：admin、密码：123456）
@@ -73,15 +73,15 @@ WHERE
 
 -- 初始化系统配置表
 CREATE TABLE IF NOT EXISTS `t_sys_config` (
-  `id` bigint(20) NOT NULL AUTO_INCREMENT,
-  `key` varchar(128) COLLATE utf8_unicode_ci NOT NULL,
-  `value` text COLLATE utf8_unicode_ci,
-  `name` varchar(32) COLLATE utf8_unicode_ci NOT NULL,
-  `description` varchar(128) COLLATE utf8_unicode_ci DEFAULT NULL,
-  `create_time` bigint(20) NOT NULL,
-  `update_time` bigint(20) NOT NULL,
-  PRIMARY KEY (`id`),
-  UNIQUE KEY `key` (`key`)
+    `id` bigint(20) NOT NULL AUTO_INCREMENT,
+    `key` varchar(128) COLLATE utf8_unicode_ci NOT NULL,
+    `value` text COLLATE utf8_unicode_ci,
+    `name` varchar(32) COLLATE utf8_unicode_ci NOT NULL,
+    `description` varchar(128) COLLATE utf8_unicode_ci DEFAULT NULL,
+    `create_time` bigint(20) NOT NULL,
+    `update_time` bigint(20) NOT NULL,
+    PRIMARY KEY (`id`),
+    UNIQUE KEY `key` (`key`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
 -- 初始化系统配置数据
@@ -186,25 +186,25 @@ INSERT INTO t_sys_config (
     `description`,
     `create_time`,
     `update_time`
-  )
+)
 SELECT
-  'siteNavs',
-  '[{\"title\":\"首页\",\"url\":\"/\"},{\"title\":\"话题\",\"url\":\"/topics\"},{\"title\":\"文章\",\"url\":\"/articles\"}]',
-  '站点导航',
-  '站点导航',
-  1555419028975,
-  1555419028975
+    'siteNavs',
+    '[{\"title\":\"首页\",\"url\":\"/\"},{\"title\":\"话题\",\"url\":\"/topics\"},{\"title\":\"文章\",\"url\":\"/articles\"}]',
+    '站点导航',
+    '站点导航',
+    1555419028975,
+    1555419028975
 FROM DUAL
 WHERE
-  NOT EXISTS (
-    SELECT
-      `key`,
-      `value`,
-      `name`,
-      `description`,
-      `create_time`,
-      `update_time`
-    FROM t_sys_config
-    WHERE
-      `key` = 'siteNavs'
-  );
+    NOT EXISTS (
+        SELECT
+            `key`,
+            `value`,
+            `name`,
+            `description`,
+            `create_time`,
+            `update_time`
+        FROM t_sys_config
+        WHERE
+            `key` = 'siteNavs'
+    );
